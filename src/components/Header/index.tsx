6835e1a2bffc9fb53cee8f826f04dd7f1cd9ad67
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-06-29 11:32:52 -0300

    add error handling for bad request api

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,15 +7,21 @@ import searchIcon from '../../assets/search-icon.png';
 
 const Header = ({ bg }: headerProp) => {
   const [pokemon, setPokemon] = useState<string>('');
+  const [badRequest, setBadRequest] = useState<boolean>(false);
 
   const context = useContext<PropsPokeContext | null>(pokeContext);
   const id = context?.pokeInfos?.id;
 
   const searchPokemon = async (pokemon: string): Promise<void> => {
-    const result = await getPokeInfos(pokemon);
-    console.log(result);
-    
-    context?.setPokeInfos(result);
+    try {
+      const result = await getPokeInfos(pokemon);
+      context?.setPokeInfos(result);
+    } catch (err) {
+      setBadRequest(true);
+      setTimeout(() => {
+        setBadRequest(false);
+      }, 1500);
+    }
   };
 
   return (
@@ -23,26 +29,36 @@ const Header = ({ bg }: headerProp) => {
       className="h-[80px] flex items-center justify-center fixed top-0 w-[100%] lg:relative"
       style={{ backgroundColor: bg }}
     >
-      <span className="mr-[30px] text-[18px] text-white">
-        {id && Number(id) > 9 ? `#0${id}` : `#00${id}`}
-      </span>
-      <input
-        className="h-[38px] w-[250px] rounded-md text-[18px] p-3"
-        type="text"
-        placeholder="search a pokemon:"
-        onChange={(e) => {
-          setPokemon(e.target.value);
-        }}
-      />
-      <button
-        className="w-[40px] ml-[-35px]"
-        type="button"
-        onClick={() => {
-          searchPokemon(pokemon);
-        }}
-      >
-        <img src={searchIcon} alt="button search" className="h-[26px]" />
-      </button>
+      <div className="flex flex-col items-center">
+        <div className="flex items-center">
+          <span className="mr-[30px] text-[18px] text-white">
+            {id && Number(id) > 9 ? `#0${id}` : `#00${id}`}
+          </span>
+          <input
+            className="h-[38px] w-[250px] rounded-md text-[18px] p-3"
+            type="text"
+            placeholder="search a pokemon:"
+            onChange={(e) => {
+              setPokemon(e.target.value);
+            }}
+          />
+          <button
+            className="w-[40px] h-[26px] ml-[-35px]"
+            type="button"
+            onClick={() => {
+              searchPokemon(pokemon);
+            }}
+          >
+            <img src={searchIcon} alt="button search" className="h-[26px]" />
+          </button>
+        </div>
+        <p
+          className="text-red-900"
+          style={{ visibility: badRequest ? 'visible' : 'hidden' }}
+        >
+          Sorry, pokemon not found
+        </p>
+      </div>
     </header>
   );
 };
