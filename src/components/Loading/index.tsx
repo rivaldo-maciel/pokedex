af9e6959f6adfa54fb557da9f53f7c495c7f9bcd
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-09-19 12:59:16 -0300

    fix loading opacity

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,7 +2,7 @@ import SpinPokeBall from '../../assets/pokeball-loading.png';
 
 const Loading = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-[100vh]">
+    <div className="flex flex-col items-center justify-center h-[100vh] opacity-[0.5]">
       <img
         src={SpinPokeBall}
         alt="pokeball"
